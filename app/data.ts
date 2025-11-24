c62a1e63a34a0527f9fd9ac67ffdeacaa78116f2
jaywoongjeong <jaywoong.jeong@gmail.com>
2025-11-24 23:36:44 +0900

    Add Digital Garden link and update social links order

--- app/data.ts
+++ app/data.ts
@@ -337,6 +337,10 @@ export const SOCIAL_LINKS: SocialLink[] = [
     link: 'https://www.linkedin.com/in/jaywoong-jeong/',
   },
   {
+    label: 'Digital Garden',
+    link: 'https://knowledge.jaywoong.me',
+  },
+  {
     label: 'Instagram',
     link: 'https://www.instagram.com/jaywoong.jeong',
   },
